0362a330e31b608de0a6b8db61d98bc1b8b23044
Mario <mro.garbo@gmail.com>
2024-09-20 09:32:25 +0800

    Replacing p tag to h1 for hierarchy also for seo

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -39,7 +39,7 @@ export default function Hero() {
             </motion.span>
           </div>
         </div>
-        <motion.p
+        <motion.h1
           className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
           initial={{ opacity: 0, y: 100 }}
           animate={{ opacity: 1, y: 0 }}
@@ -50,7 +50,7 @@ export default function Hero() {
           enjoy building{" "}
           <span className="italic">dynamic sites and applications</span>. My
           stack is <span className="underline">MENR</span>.
-        </motion.p>
+        </motion.h1>
 
         <motion.div
           className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
